158686065e5ca4ae06edce5b06af45f7832ea236
Vincent Chen <vincentchenbwork@gmail.com>
2023-06-15 12:43:18 +0800

    安安你好 我是 Java 路人甲 路過 GitHub

---
+++ GitHubHelloWorld.java
@@ -0,0 +1,13 @@
+package HelloWorld;
+
+public class GitHubHelloWorld {
+	public static void main(String[] args) {
+		
+		String lol = "Java 開發者路過".concat(" 安安你好 我叫 Hello World");
+				
+		
+		System.out.println(lol);
+		
+		
+	}
+}
